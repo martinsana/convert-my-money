9b270569a4c951d47b6ddc8f7b979782c2781a0f
Ana Paula Martins <anapaula.martins17@hotmail.com>
2019-06-14 14:56:06 -0300

    Adicionando requisicao direta do BCB

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app= express()
 const path = require('path')
 
 const convert = require('./lib/convert')
+const apiBCB = require('./lib/api.bcb')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
@@ -10,8 +11,11 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 
 
-app.get('/', (req, res) => {
-    res.render('home')
+app.get('/', async(req, res) => {
+    const cotacao = await apiBCB.getCotacao()    
+    res.render('home', {
+        cotacao
+    })    
 })
 
 
